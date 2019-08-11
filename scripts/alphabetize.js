01838396ec9ffcc6b4caade00af038653f5ce3b1
James Jensen <jamesarthurjensen@gmail.com>
2019-08-11 20:00:58 +0800

    Add Continuous Integration

---
+++ scripts/alphabetize.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const { readFileSync, writeFileSync } = require("fs");
+const { resolve } = require("path");
+
+const termsFile = resolve(__dirname, "../terms.txt");
+
+const termsString = readFileSync(termsFile).toString();
+const sortedTerms = termsString.replace(/,*\n*$/, "").split(",\n").sort();
+const sortedTermsString = sortedTerms.join(",\n")+"\n"
+
+if (sortedTermsString !== termsString) {
+  console.error("Not in correct order")
+  if(process.argv.includes("--print")) {
+    console.log(`\`\`\`
+${sortedTermsString}
+\`\`\``);
+  }
+  if (process.argv.includes("--write")) {
+    writeFileSync(termsFile, sortedTermsString);
+  }
+} else {
+  console.log("Already in alphabetical order");
+}
